bbc26eaa2b719418da096d4b1d35c4e5fefdeaad
filipe-fff <pintovicentinifilipe@gmail.com>
2024-08-14 18:59:29 -0300

    operadores aritmetricos

---
+++ java-basico/aulas/JavaBasico/src/edu/filipe/javaBasico/operadores/aula012/OperadoresAritmetricos.java
@@ -0,0 +1,14 @@
+package edu.filipe.javaBasico.operadores.aula012;
+
+public class OperadoresAritmetricos
+{
+    public static void main(String[] args)
+    {
+        int numero = 5;
+        numero = - numero;
+        System.out.println(numero);
+
+        numero = - numero;
+        System.out.println(numero);
+    }
+}
