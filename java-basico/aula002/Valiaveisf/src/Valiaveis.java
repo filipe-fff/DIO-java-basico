d37d8ffb668cb9100045f40e11c42e55ea5fa2d0
filipe-fff <pintovicentinifilipe@gmail.com>
2024-08-14 01:56:46 -0300

    valiaveis feito

---
+++ java-basico/aula002/Valiaveisf/src/Valiaveis.java
@@ -0,0 +1,20 @@
+public class Valiaveis {
+    public static void main(String[] args) {
+        /*
+         *    Valiaveis só podem terem simbolos _ e $, não
+         * podem iniciarem com números e não podem se chamarem
+         * com palavras reservadas.
+         */
+
+        // valiaveis nós colocamos em a primeira letra da palavra em maiúscula,
+        // exeto a primeira palavra.
+        float precoMercado = 2.f;
+
+
+        // em constantes serve para valores que não mudam, acressentando final,
+        // e nós colocamos tudo em maiúcula e separado _ .
+        final double PI = 3.1415;
+
+        final String NOME = "Filipe";
+    }
+}
