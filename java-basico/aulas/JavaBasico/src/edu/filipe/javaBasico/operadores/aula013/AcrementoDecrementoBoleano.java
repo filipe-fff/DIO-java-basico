5d3f2bc78e1d8a3c0c531fa21dbd937e83a77e2a
filipe-fff <pintovicentinifilipe@gmail.com>
2024-08-14 19:30:37 -0300

    Acremento, decremento, booleano e ternario feito

---
+++ java-basico/aulas/JavaBasico/src/edu/filipe/javaBasico/operadores/aula013/AcrementoDecrementoBoleano.java
@@ -0,0 +1,14 @@
+package edu.filipe.javaBasico.operadores.aula013;
+
+public class AcrementoDecrementoBoleano {
+    public static void main(String[] args)
+    {
+        int numero = 5;
+        System.out.println(numero++);
+        System.out.println(numero);
+        System.out.println(++numero);
+
+        boolean valor = true;
+        System.out.println(!valor);
+    }
+}
---
+++ java-basico/aulas/JavaBasico/src/edu/filipe/javaBasico/operadores/aula014/Ternario.java
@@ -0,0 +1,12 @@
+package edu.filipe.javaBasico.operadores.aula014;
+
+public class Ternario
+{
+    public static void main(String[] args)
+    {
+        int a = 7;
+        int b = 7;
+        String resultado = a == b ? "verdadeiro":"falso" ;
+        System.out.println(resultado);
+    }
+}
