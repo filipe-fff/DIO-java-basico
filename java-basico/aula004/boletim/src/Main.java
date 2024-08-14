686642a50a404ca260a740e382fc3e1102e5cba6
filipe-fff <pintovicentinifilipe@gmail.com>
2024-08-14 03:08:26 -0300

    boletim feito

---
+++ java-basico/aula004/boletim/src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int nota = 10;
+        if(nota > 6) System.out.println("APROVADO!");
+
+        else if(nota == 6) System.out.println("RECUPERAÇÃO!");
+        
+        else System.out.println("REPROVADO!");
+    }
+}
