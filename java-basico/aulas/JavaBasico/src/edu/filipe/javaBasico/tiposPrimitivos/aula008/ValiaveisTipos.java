ad1ae8b13a0ae9802f05c766b7bd13ac60f31832
filipe-fff <pintovicentinifilipe@gmail.com>
2024-08-14 18:18:07 -0300

    tipos primitivos finalizado

---
+++ java-basico/aulas/JavaBasico/src/edu/filipe/javaBasico/tiposPrimitivos/aula008/ValiaveisTipos.java
@@ -0,0 +1,15 @@
+package edu.filipe.javaBasico.tiposPrimitivos.aula008;
+
+public class ValiaveisTipos {
+    public static void main(String [] args)
+    {
+        // valiavel
+        int numero = 5;
+        numero = 10;
+        System.out.println(numero);
+
+        // constante
+        final double VALOR_DE_PI = 3.1415;
+        System.out.println(VALOR_DE_PI);
+    }
+}
