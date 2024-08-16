2b117d2ff6bdcbce5dc0f4d1d9a9ed45ca4be4ed
filipe-fff <pintovicentinifilipe@gmail.com>
2024-08-16 17:30:02 -0300

    ok

---
+++ java-basico/aulas/JavaBasico/src/edu/filipe/javaBasico/palavrasReservadas/aula025/Calculadora.java
@@ -0,0 +1,25 @@
+package edu.filipe.javaBasico.palavrasReservadas.aula025;
+/*
+ * <h1>Calculadora</h1>
+ *
+ * <p> A <strong>Calculadora</strong> ela é um método que recebe os valores em parametros n1 e n2 para retornarem um valor que é a soma entre os dois. </p>
+ *
+ * <hr/>
+ *
+ *  @autor Filipe
+ *  @version 1.0
+ *  @since 16/08/2024
+ */
+public class Calculadora
+{
+    /*
+     *  @param n1 primeiro número
+     *  @param n2 segundo número
+     *  @return retorna a soma entre os parametro n1 e n2
+     *
+     * */
+    public int somar(int n1, int n2)
+    {
+        return n1+n2;
+    }
+}
---
+++ java-basico/aulas/JavaBasico/src/edu/filipe/javaBasico/palavrasReservadas/aula025/Main.java
@@ -0,0 +1,11 @@
+package edu.filipe.javaBasico.palavrasReservadas.aula025;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        Calculadora calculadora = new Calculadora();
+        int soma = calculadora.somar(1, 3);
+        System.out.println(soma);
+    }
+}
